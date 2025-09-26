1c74451ab09d1c3962c87eb64055015b7b24ac67
Biplov Thapa <biplovthapa@Biplovs-MacBook-Pro.local>
2025-09-26 22:39:41 +0545

    added custom domain

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL=`https://a84s480c4kgck000og0kkc88.139.59.16.50.sslip.io/api/v1`
+export const BACKEND_URL=`https://biplov.work.gd/api/v1`
 export function getToken(){
     const token = sessionStorage.getItem("auth_token")
     if (!token) {
