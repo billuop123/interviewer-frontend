74032d250e7a18c1410f0930331e38b64fbc7487
Biplov Thapa <biplovthapa@Biplovs-MacBook-Pro.local>
2025-09-27 09:01:27 +0545

    new commit

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL=`https://biplov.work.gd/api/v1`
+export const BACKEND_URL=`https://biplov369.work.gd/api/v1`
 export function getToken(){
     const token = sessionStorage.getItem("auth_token")
     if (!token) {
