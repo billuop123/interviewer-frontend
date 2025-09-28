3438fb7d47e924b06fe37905843dd605f7ae0d22
Biplov Thapa <biplovthapa@Biplovs-MacBook-Pro.local>
2025-09-28 16:29:31 +0545

    updated frontend

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL=`https://biplov.run.place/api/v1`
+export const BACKEND_URL=`https://billu.work.gd/api/v1`
 export function getToken(){
     const token = sessionStorage.getItem("auth_token")
     if (!token) {
